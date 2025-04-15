f0c9d15100c7bad9174bdfd3fcf373a416351c7f
Jullie <Julliebertcogueron021@gmail.com>
2025-04-16 00:19:06 +0800

    Login Form Revision Done

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ document.getElementById("LoginBTN").addEventListener('click', function(e) {
     console.log("Loading animation started");
 });
 
+let RoleToggle = "Customer";
+const toggleroleBTN = document.querySelector(".navtop label")
+
+
 const selecbtn = document.querySelectorAll(".selectionbtn")
 const selectiondivs = document.querySelectorAll(".selectiondivs")
 
@@ -20,6 +24,46 @@ selecbtn.forEach(i =>{
         let target = i.dataset.target
         document.querySelector(`.${target}`).classList.remove("hidediv")
     } )
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    if(RoleToggle == "Customer"){
+        document.querySelector(".forcustomers").classList.remove("hidediv")
+        document.querySelector(".foradmin").classList.add("hidediv")
+        document.querySelector(".navtop label").innerHTML = "Login as Admin or Staff"
+    }
+    else{
+        document.querySelector(".forcustomers").classList.remove("hidediv")
+        document.querySelector(".foradmin").classList.add("hidediv")
+         document.querySelector(".navtop label").innerHTML = "Register/Login as Customer"
+
+    }
+})
+
+const centerdivs = document.querySelectorAll(".centerdiv")
+
+toggleroleBTN.addEventListener('click', function(){
+
+    if(RoleToggle == "Customer"){
+        centerdivs.forEach(element => {
+            element.classList.remove("hidediv")
+        });
+        document.querySelector(".foradmin").classList.add('hidediv')
+        document.querySelector(".navtop label").innerHTML = "Login as Admin or Staff"
+        RoleToggle = "Admin"
+        console.log(RoleToggle)
+    }
+    else{
+        centerdivs.forEach(element => {
+            element.classList.remove("hidediv")
+        });
+        document.querySelector(".forcustomers").classList.add("hidediv")
+        document.querySelector('.foradmin .forlogin').classList.remove('hidediv')
+         document.querySelector(".navtop label").innerHTML = "Register/Login as Customer"
+        RoleToggle = "Customer"
+        console.log(RoleToggle)
+    }
 })
 
 
